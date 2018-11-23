5f71c03449fb70056c3d6891583b7b5d2dfac2e5
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-11-23 13:07:15 -0500

    t2 for graph 1 and 2

--- t2.py
+++ t2.py
@@ -10,7 +10,7 @@ bashCommand = {}
 
 showCommand = " ndnping /ndn/metrics/show -i 1 -c 1 -n 1234567 -o 1"
 zeroCommand = " ndnping /ndn/metrics/zero -i 1 -c 1 -n 1234567 -o 1"
-resultDir = " >> /home/lenovo/Dropbox/Thesis/Logs/minindn4/clientLogs_1.txt"
+resultDir = " >> /home/amaldar/clientLogs_1.txt"
 
 # sample test pattern
 # pingServer =  d ndnpingserver /ndn/d-site/d/prefix4/prefix5/prefix6/prefix7/prefix8/prefix9/prefix10 -x 1000000 &> /home/lenovo/Dropbox/Thesis/Logs/minindn3/serverLogs.txt &
