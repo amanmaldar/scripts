3c8788b130ff54c9f599b2eeeb3533864db8e08b
Aman Maldar <amanmaldar@users.noreply.github.com>
2018-11-23 13:34:41 -0500

    fixes to t2

--- t2.py
+++ t2.py
@@ -12,9 +12,9 @@ showCommand = " ndnping /ndn/metrics/show -i 1 -c 1 -n 1234567 -o 1"
 zeroCommand = " ndnping /ndn/metrics/zero -i 1 -c 1 -n 1234567 -o 1"
 resultDir = " >> /home/amaldar/clientLogs_1.txt"
 
-# sample test pattern
-# pingServer =  d ndnpingserver /ndn/d-site/d/prefix4/prefix5/prefix6/prefix7/prefix8/prefix9/prefix10 -x 1000000 &> /home/lenovo/Dropbox/Thesis/Logs/minindn3/serverLogs.txt &
-# a ndnping /ndn/d-site/d/prefix4/prefix5/prefix6/prefix7/prefix8/prefix9/prefix10 -i 1 -c 1 -n 777777
+# sample test pattern. This should pass before starting test
+# pingServer =  ndnpingserver /ndn/d-site/d/prefix4/prefix5/prefix6/prefix7/prefix8/prefix9/prefix10 -x 1000000 &> /home/amaldar/serverLogs.txt &
+# ndnping /ndn/d-site/d/prefix4/prefix5/prefix6/prefix7/prefix8/prefix9/prefix10 -i 1 -c 1 -n 777777
 
 
 print "Starting the pings"
@@ -24,6 +24,7 @@ bashCommand[0] = zeroCommand
 #measurement 1
 bashCommand[1] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 1"				# request 5000 packets and call show
 bashCommand[2] = showCommand	# miss
+bashCommand[0] = zeroCommand
 bashCommand[3] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 1"				# request same 5000 packets and call show
 bashCommand[4] = showCommand	# hit
 
@@ -33,6 +34,7 @@ bashCommand[6] = zeroCommand
 #measurement 2
 bashCommand[7] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 10001"			# request 5000 packets and call show   
 bashCommand[8] = showCommand 	# miss
+bashCommand[0] = zeroCommand
 bashCommand[9] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 10001"			# request same 5000 packets and call show   
 bashCommand[10] = showCommand	# hit											
 
@@ -42,6 +44,7 @@ bashCommand[12] = zeroCommand
 #measurement 3
 bashCommand[13] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 25001"			# request 5000 packets and call show 
 bashCommand[14] = showCommand	# miss													 
+bashCommand[0] = zeroCommand
 bashCommand[15] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 25001"			# request same 5000 packets and call show 
 bashCommand[16] = showCommand	# hit										
 
@@ -51,6 +54,7 @@ bashCommand[18] = zeroCommand
 #measurement 4
 bashCommand[19] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 40001"			# request 5000 packets and call show
 bashCommand[20] = showCommand	# miss
+bashCommand[0] = zeroCommand
 bashCommand[21] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 40001"			# request same 5000 packets and call show
 bashCommand[22] = showCommand	# hit										
 
@@ -60,6 +64,7 @@ bashCommand[24] = zeroCommand
 #measurement 5
 bashCommand[25] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 55001"			# request 5000 packets and call show
 bashCommand[26] = showCommand	# miss
+bashCommand[0] = zeroCommand
 bashCommand[27] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 55001"			# request same 5000 packets and call show
 bashCommand[28] = showCommand	# hit 										
 
@@ -69,6 +74,7 @@ bashCommand[30] = zeroCommand
 #measurement 6
 bashCommand[31] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 66001"			# request 5000 packets and call show
 bashCommand[32] = showCommand 	# miss
+bashCommand[0] = zeroCommand
 bashCommand[33] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 66001"			# request same 5000 packets and call show
 bashCommand[34] = showCommand 	# hit
 
@@ -77,6 +83,7 @@ bashCommand[34] = showCommand 	# hit
 bashCommand[35] = zeroCommand
 bashCommand[36] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 73000"			# request 5000 packets and call show
 bashCommand[37] = showCommand	# miss
+bashCommand[0] = zeroCommand
 bashCommand[38] = " ndnping " + interestPrefix + " -i 1 -c 5000 -n 73000"			# request 5000 packets and call show
 bashCommand[39] = showCommand	# hit									
 
